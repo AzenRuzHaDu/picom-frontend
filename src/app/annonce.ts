b843d675d1b07038b874cfc8e0c729b1acc6517f
Mad <madiane.lenigen@protonmail.com>
2022-08-26 00:55:40 +0200

    started coding business classes on this branch, switching to dev then feature one

---
+++ src/app/annonce.ts
@@ -0,0 +1,16 @@
+export class Annonce {
+
+  id?:number;
+  dateHeureCreation?:Date;
+  dateHeureDebut?:Date;
+  dateHeurefin?:Date;
+  contenu?:String;
+  titre?:String;
+  numeroCarte?:String;
+  anneExpiration?:number;
+  moisExpiration?:number;
+  cryptogramme?:String;
+  montantRegleEnEuros?:number;
+//TODO: add List of Zone tranche horraires & add one client
+
+}
---
+++ src/app/zone.ts
@@ -0,0 +1,11 @@
+import { Annonce } from "./annonce";
+
+export class Zone {
+
+
+  id?:number;
+  nom?:String;
+  annonces?:Annonce[]
+  //TODO: add list of arrets
+
+}
