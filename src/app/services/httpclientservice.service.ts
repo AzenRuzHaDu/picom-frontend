83751bf2c193549b621d2a828eac711c43c5dc09
Mad <madiane.lenigen@protonmail.com>
2022-08-29 01:08:55 +0200

    adding httpservice with any type to factor

--- src/app/services/httpclientservice.service.ts
+++ src/app/services/httpclientservice.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Client } from '../business/client';
+import { catchError, Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -11,13 +10,31 @@ export class HttpclientserviceService {
 
   constructor(private clientHttp: HttpClient) {}
 
-  getAll(pathEnd: string): Observable<any[]>{
+  getAll(pathToApi: string): Observable<any[]>{
     // TODO document why this method 'getAll' is empty
-return this.clientHttp.get<any[]>(`http://localhost:8080/api/${pathEnd}`)
+return this.clientHttp.get<any[]>(`http://localhost:8080/api/${pathToApi}`)
 
   }
 
+  getOne(pathToAPI: string, objectId: string): Observable<any> {
+    return this.clientHttp.get<any>(`http://localhost:8080/api/${pathToAPI}/${objectId}`).pipe(
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
 
 
+  addOne(objectToAdd: any | undefined): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
 
+    return this.clientHttp.post<any>(`http://localhost:8080/api/${objectToAdd}`, objectToAdd, httpOptions).pipe(
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    return of(errorValue);
+  }
 }
+
